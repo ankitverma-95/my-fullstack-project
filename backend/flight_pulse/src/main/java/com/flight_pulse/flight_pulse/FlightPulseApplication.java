3053950c5e0d97d5ef2c1a6e258b38334ba342e8
ankit <av6007616@gmail.com>
2024-07-26 14:08:02 +0530

    Initial setup of frontend and backend

---
+++ backend/flight_pulse/src/main/java/com/flight_pulse/flight_pulse/FlightPulseApplication.java
@@ -0,0 +1,13 @@
+package com.flight_pulse.flight_pulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPulseApplication.class, args);
+	}
+
+}
---
+++ backend/flight_pulse/src/test/java/com/flight_pulse/flight_pulse/FlightPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight_pulse.flight_pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
