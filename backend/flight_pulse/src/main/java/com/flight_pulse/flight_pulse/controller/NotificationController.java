b95abcdf1c2beb7672997abb79d7849cf495ae49
ankit <av6007616@gmail.com>
2024-07-27 13:29:45 +0530

    Send notification by fcm token function has successfully created

--- backend/flight_pulse/src/main/java/com/flight_pulse/flight_pulse/controller/NotificationController.java
+++ backend/flight_pulse/src/main/java/com/flight_pulse/flight_pulse/controller/NotificationController.java
@@ -1,2 +1,22 @@
-package com.flight_pulse.flight_pulse.controller;public class NotificationController {
+package com.flight_pulse.flight_pulse.controller;
+
+import com.flight_pulse.flight_pulse.NotificationMessage;
+import com.flight_pulse.flight_pulse.service.FirebaseMessagingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+
+    @Autowired
+    FirebaseMessagingService firebaseMessagingService;
+
+    @PostMapping
+    public String sendNotificationByToken(@RequestBody NotificationMessage notificationMessage) {
+        return firebaseMessagingService.sendNotificationByToken(notificationMessage);
+    }
 }
